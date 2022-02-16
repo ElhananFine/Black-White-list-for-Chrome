9134787984a9b3eada0621c79ec59117252636f2
elhananfine <83065544+elhananfine@users.noreply.github.com>
2022-02-16 16:32:52 +0200

    Update configuration-chrome.py

--- configuration-chrome.py
+++ configuration-chrome.py
@@ -17,38 +17,3 @@ command_args = CommandArgs(
 configuration.per_page_limit = 20
 # המדד הראשון להחזרת התוצאות, (ברירת מחדל 0)
 configuration.per_page_offset = 0
-
-
-
-# קבוצות להחלת ההגדרות
-def managed_configuration_all_groups():
-    # create an instance of the API class
-    api_instance = אפליקציות כשרות.DeviceGroupApi(אפליקציות כשרות.ApiClient(configuration))
-    try:
-        api_response = api_instance.get_all_groups(enterprise_id,
-                                                    limit=configuration.per_page_limit,
-                                                    offset=configuration.per_page_offset)
-        if len(api_response.results):
-            for group in api_response.results:
-                # add all the devices in this group to global list of devices
-                run_managed_configuration(גוגל פליי כשר ודפדפן מסונן)
-        #print(allgroups)
-
-        while status.state not in ["Command Success", "Command Failure", "Command TimeOut", "Command Cancelled", "Command Queued"]:
-            response = api_instance.get_command_request_status(enterprise_id, request_id)
-            status = response.results[0]
-            time.sleep(1)
-            #print(status)
-
-    except ApiException as e:
-        print("Exception when calling CommandsV2Api->create_command: %s\n" % e)
-
-
-"""
-Main Function
-"""
-def main():
-    managed_configuration_all_groups()
-
-if __name__ == "__main__":
-    main()
